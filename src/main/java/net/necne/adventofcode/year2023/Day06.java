dce78d1df016e7275b975201d161a6532b23e415
Nathan Brewster <brewster@necne.net>
2024-01-16 10:50:21 -0600

    2023d06p2

--- src/main/java/net/necne/adventofcode/year2023/Day06.java
+++ src/main/java/net/necne/adventofcode/year2023/Day06.java
@@ -10,12 +10,12 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.logging.Level;
-import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 
 @Log
 public class Day06 {
 
-    record Race(int time, int distance){
+    record Race(long time, long distance){
     }
 
     static Set<Race> parseRaces(String resource) throws IOException {
@@ -29,29 +29,41 @@ public class Day06 {
         var races = new HashSet<Race>();
         for(int i = 1; i < times.length; ++i){
             races.add(new Race(
-                    Integer.parseInt(times[i]),
-                    Integer.parseInt(distances[i])
+                    Long.parseLong(times[i]),
+                    Long.parseLong(distances[i])
             ));
         }
         return races;
     }
 
-    static int raceDistance(int time, int hold){
-        int speed = hold;
-        return speed * (time - hold);
+    static Race parseRace(String resource) throws IOException {
+        String[] lines = Resources.toString(Resources.getResource(resource), Charset.defaultCharset())
+                .replace(" ", "")
+                .split("\n");
+        if(lines.length != 2) throw new IllegalArgumentException("Expected 2 lines and found " + lines.length);
+
+        return new Race(
+                Long.parseLong(lines[0].split(":")[1]),
+                Long.parseLong(lines[1].split(":")[1])
+        );
+    }
+
+
+    static long raceDistance(long time, long hold){
+        return hold * (time - hold);
     }
 
     static final String RACE_FORMAT = "\n  %3d  %4d";
     static void logRace(Race race) {
         StringBuilder sb = new StringBuilder();
-        IntStream.rangeClosed(0, race.time).forEach(
+        LongStream.rangeClosed(0, race.time).forEach(
                 i -> sb.append(RACE_FORMAT.formatted(i, raceDistance(race.time, i)))
         );
         log.info("Race " + race + sb);
     }
 
     static long countWinnable(Race race){
-        return IntStream.rangeClosed(0, race.time)
+        return LongStream.rangeClosed(0, race.time)
                 .filter(hold -> race.distance < raceDistance(race.time, hold))
                 .count();
     }
@@ -68,11 +80,19 @@ public class Day06 {
             var timer = new Timer();
             var sampleRaces = parseRaces("2023/06/sample");
             var puzzleRaces = parseRaces("2023/06/puzzle");
-            timer.split("load");
+            timer.split("load multiple");
             logRace(sampleRaces.stream().findFirst().orElseThrow());
-            log.info("sample x winnable " + multipleWinnable(sampleRaces));
+            log.info("sample x winnable " + multipleWinnable(sampleRaces)); //288
             timer.split();
-            log.info("puzzle x winnable " + multipleWinnable(puzzleRaces));
+            log.info("puzzle x winnable " + multipleWinnable(puzzleRaces)); //1195150
+            timer.split();
+
+            var sampleRace = parseRace("2023/06/sample");
+            var puzzleRace = parseRace("2023/06/puzzle");
+            timer.split("load single");
+            log.info("sample single winnable " + countWinnable(sampleRace)); //71503
+            timer.split();
+            log.info("puzzle single winnable " + countWinnable(puzzleRace)); //42550411
             timer.split();
         }
         catch(Exception e){
