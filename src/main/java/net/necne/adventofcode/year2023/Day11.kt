d97cbaa76d82901fdca2e5eb5e7988432e921496
Nathan Brewster <brewster@necne.net>
2024-04-08 16:49:28 -0500

    2023d11p2

--- src/main/java/net/necne/adventofcode/year2023/Day11.kt
+++ src/main/java/net/necne/adventofcode/year2023/Day11.kt
@@ -6,48 +6,60 @@ import kotlin.math.abs
 
 // https://adventofcode.com/2023/day/11
 
+data class Galaxy(var col : Long, var row : Long){
+    fun expand(col : Long, row : Long) {
+        this.col += col
+        this.row += row
+    }
+}
+
 fun print(layout: List<List<Char>>) =
     println(
         layout.map { row -> "" + row.joinToString("") }
             .joinToString("\n")
     )
 
-fun stepsBetween(a: Pos, b: Pos) = abs(a.row - b.row) + abs(a.col - b.col)
+fun stepsBetween(galaxyA: Galaxy, galaxyB: Galaxy) = abs(galaxyA.row - galaxyB.row) + abs(galaxyA.col - galaxyB.col)
 
-fun parseGalaxy(resource: String) {
+fun parseGalaxy(resource: String, expansion: Int) {
+    val netExpansion = (expansion - 1).toLong()
     val layout = Resources.readLines(Resources.getResource(resource), Charset.defaultCharset())
         .map {it.toCharArray().toMutableList() }
         .toMutableList()
+    print(layout)
+
+    val galaxys = mutableListOf<Galaxy>()
+    for( row in 0..<layout.size )
+        for( col in 0..<layout.first().size)
+            if( layout[row][col] == '#' ) galaxys.add(Galaxy(col.toLong(), row.toLong()))
+    println("initial galaxys\n$galaxys")
 
     for ( row in layout.size-1 downTo 0 )
         if (layout[row].none { it == '#' }) {
-            println("no galaxy in row $row")
-            layout.add(row, layout[row].toMutableList())
+            galaxys.filter { it.row > row }.forEach { it.expand(0, netExpansion) }
         }
 
     for ( col in layout.first().size-1 downTo 0)
         if(layout.none { it[col] == '#' }) {
-            println("no galaxy in col $col")
-            layout.map { it.add(col, '.')  }
+            galaxys.filter { it.col > col }.forEach { it.expand(netExpansion, 0) }
         }
-    print(layout)
 
-    val poss : MutableList<Pos> = mutableListOf()
-    for( row in 0..<layout.size)
-        for( col in 0..<layout.first().size)
-            if(layout[row][col] == '#') poss.add(Pos(col, row))
-
-    var sumShortestPaths = 0
-    for( i in 0..<poss.size){
-        for( j in i..<poss.size) {
-            sumShortestPaths += stepsBetween(poss[i], poss[j])
-            println("$i $j ${stepsBetween(poss[i], poss[j])}")
+    var sumShortestPaths = 0L
+    for( i in 0..<galaxys.size){
+        for( j in i..<galaxys.size) {
+            sumShortestPaths += stepsBetween(galaxys[i], galaxys[j])
+            println("$i $j ${stepsBetween(galaxys[i], galaxys[j])}")
         }
     }
     println("sum of shortest paths: $sumShortestPaths")
 }
 
 fun main() {
-    parseGalaxy("2023/11/sample") // 374
-    parseGalaxy("2023/11/puzzle") // 9274989
+    //part 1
+    parseGalaxy("2023/11/sample", 2) // 374
+    parseGalaxy("2023/11/puzzle", 2) // 9274989
+    //part 2
+    parseGalaxy("2023/11/sample", 10) // 1030
+    parseGalaxy("2023/11/sample", 100) // 8410
+    parseGalaxy("2023/11/puzzle", 1000000) // 357134560737
 }
